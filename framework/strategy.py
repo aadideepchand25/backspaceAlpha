7b071b1326198a47aeb3063d42f4f06bae4600b0
Aadi Deepchand <aadideepchand8254@gmail.com>
2025-07-01 00:16:43 +0100

    Updated Strategy base class to require portfolio and name and methods

--- framework/strategy.py
+++ framework/strategy.py
@@ -1,12 +1,17 @@
+from abc import ABC, abstractmethod
+
 #Base Class for strategies
-class Strategy:
-    def __init__(self):
-        self.portfolio = []
+class Strategy(ABC):
+    def __init__(self, portfolio, name):
+        self.portfolio = portfolio
         self.broker = None
         self.feed = None
-        
+        self.name = name
+    
+    @abstractmethod
     def init(self):
         pass
 
+    @abstractmethod
     def update(self, data):
-        raise NotImplementedError 
+        pass
