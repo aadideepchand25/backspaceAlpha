f46b6136f83b0ab15c764478fd444786f27bf069
Aadi Deepchand <aadideepchand8254@gmail.com>
2025-09-18 02:12:34 +0100

    Added a basic mean reversion strategy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from strategies.PairsTradingStrategy import PairsTradingStrategy
+from strategies.MeanReversionStrategy import MeanReversionStrategy
 import framework as f
 
 '''
@@ -6,10 +6,10 @@ Quick example to show how to use the backtester to run the simulation and then s
 Can process a backtest of 5 years on a 1 day timeframe in about a few seconds with a large portfolio
 '''
 
-backtest = f.BackTest(PairsTradingStrategy(), ('2000-01-01', '2025-01-01'), 10000, "YAHOO", "1D", verbose=False)
+backtest = f.BackTest(MeanReversionStrategy(), ('2000-01-01', '2025-01-01'), 10000, "YAHOO", "1D", verbose=False)
 backtest.run()
 backtest.show_portfolio()
 #backtest.show_portfolio_distribution()
 backtest.show_results()
 #backtest.show_stock("PEP")
-#backtest.show_stock("KO")
+backtest.show_stock("SPY")
---
+++ strategies/MeanReversionStrategy.py
@@ -0,0 +1,33 @@
+from framework import Strategy
+import numpy as np
+
+class MeanReversionStrategy(Strategy):
+    def __init__(self):
+        super().__init__(["SPY"], self.__class__.__name__)
+    
+    def init(self):
+        self.window_size = 25
+        self.threshold = 2
+        self.exit = 0.5
+        self.shares = 5
+    
+    def update(self, data):
+        #Get close prices
+        price = data[0][3]
+        
+        #Get rolling window
+        previous_prices = self.feed.previous(self.window_size)[:,:,3]
+        mean = np.mean(previous_prices)
+        std = np.std(previous_prices)
+        
+        #Calculate z-score
+        z = (price - mean)/(std)
+        
+        #Make trades
+        if z > self.threshold:
+            self.broker.short("SPY", self.shares, "short_order")
+        elif z < -self.threshold:
+            self.broker.long("SPY", self.shares, "long_order")
+        elif z < self.exit and z > -self.exit:
+            self.broker.close("short_order")
+            self.broker.close("long_order")
